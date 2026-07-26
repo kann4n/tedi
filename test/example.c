b5b012659cc1990627f29e5f30dfe6f60edc5798
Aravind K.B <kannan.codes456@gmail.com>
2026-07-26 20:35:41 +0530

    Create testfile made a playground to test out tedi

---
+++ test/example.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  printf("\033[4;31mTedi\033[0m\n");
+  printf("Simple Text Editor\n");
+  printf("\n\n");
+  printf("This code is just for testing out if the tedi editor is working");
+  // comment
+  /*
+   * this is 
+   * a
+   * mutiline
+   * commnet
+   */
+  return 0;
+}
