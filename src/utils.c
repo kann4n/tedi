5da8cf30b1076c0cd8c376f1acce12d03e7d59cf
Aravind K.B <kannan.codes456@gmail.com>
2026-07-28 19:01:38 +0530

    update utils
    added helping functions for setting terminal color using RGB.

--- src/utils.c
+++ src/utils.c
@@ -1,16 +1,28 @@
 #include "utils.h"
+#include <stdio.h>
 #include <string.h>
 
+
 int getfilename(char *dst, const char *src, int maxlen) {
-	char *slash = strrchr(src, '/');
-	const char *name_start = slash ? slash : src;
-	char *name_end = strrchr(src, '.');
-	
-	int name_len = name_end ? (int)(name_end - name_start) : strlen(name_start);
-	if (name_len >= maxlen) name_len = maxlen - 1;
+  char *slash            = strrchr(src, '/');
+  const char *name_start = slash ? slash : src;
+  char *name_end         = strrchr(src, '.');
 
-	strncpy(dst, name_start, name_len);
-	dst[name_len] = '\0';
+  int name_len = name_end ? (int)(name_end - name_start) : (int)strlen(name_start);
+  if (name_len >= maxlen)
+    name_len = maxlen - 1;
 
-	return name_len;
+  strncpy(dst, name_start, name_len);
+  dst[name_len] = '\0';
+
+  return name_len;
 }
+
+void setColorFG(char *fg, char r, char g, char b) {
+  snprintf(fg, 24, "\033[38;2;%d;%d;%dm", r, g, b);
+}
+
+void setColorBG(char *bg, char r, char g, char b) {
+  snprintf(bg, 24, "\033[48;2;%d;%d;%dm", r, g, b);
+}
+
--- src/utils.h
+++ src/utils.h
@@ -2,14 +2,36 @@
 #define UTILS_H
 
 /*
- * @brief store filename in dst from src with cap of maxlen
+ * Extracts the filename from a given path.
  *
- * @param dst
- * @param src
- * @param maxlen
+ * \param destination buffer to store the filename.
+ * \param source path from which to extract the filename.
+ * \param maximum length of the filename to extract.
+ * \return length of the extracted filename.
  *
- * @return length of filename
+ * note: path must be in the unix format.
  */
 int getfilename(char *dst, const char *src, int maxlen);
+
+
+/*
+ * Sets the foreground color of the terminal.
+ *
+ * \param buffer to store the color code.
+ * \param red component of the color.
+ * \param green component of the color.
+ * \param blue component of the color.
+ */
+void setColorFG(char *fg, char r, char g, char b);
+
+/*
+ * Sets the background color of the terminal.
+ *
+ * \param buffer to store the color code.
+ * \param red component of the color.
+ * \param green component of the color.
+ * \param blue component of the color.
+ */
+void setColorBG(char *bg, char r, char g, char b);
 
 #endif
